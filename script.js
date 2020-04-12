406260364836dcce75379952959b1c75fbf38770
Emily Wirtz <ewirtz3@gmail.com>
2020-04-12 13:12:16 -0500

    removing console logs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//add time blocks, in one hour increments from 9am to 5pm, each block has a blue save button on the right hand side that saves the info entered to local storage
+//add time blocks, in one hour increments from 9am to 5pm, each block has a save button on the right hand side that saves the info entered to local storage
 
 //add current day at the top of the planner (moment)
 var currentTime = moment().format("MMMM Do YYYY");
@@ -6,13 +6,11 @@ $("#currentDay").text(currentTime);
 
 //variables for now (as a baseline time, use moment) and HTML time block (to change color relative to now)
 var timeBlock = $(".hour");
-console.log(timeBlock);
 var now = parseInt(moment().format("H"));
 
-//function to check each hour block to see if it is past (gray), present (red), or future (green). parseInt(hourId) <>= now doesn't work, 10 > 1 but 10am is before 1pm...maybe try comparing index in hour array? if hour[i]><=hour[j]; how to do that?
+//function to check each hour block to see if it is past (gray), present (red), or future (green).
 $.each(timeBlock, function (i, hour) {
   var hourId = parseInt($(this).attr("id"));
-  console.log(hourId);
   if (hourId === now) {
     $(this).addClass("present");
   } else if (hourId < now) {
@@ -32,4 +30,8 @@ $(".saveBtn").on("click", function (event) {
   // $(".event").append(event);
 });
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  for (var i = 0; i < localStorage.length; i++) {
+    console.log(localStorage.getItem(localStorage.key(i)));
+  }
+});
